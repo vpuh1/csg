0bdd64245bfed5d152dcdc8ae39af90c9cc1d1ed
vpuh1 <pro100alinchik@gmail.com>
2021-05-23 18:29:33 +0000

    fixed titles

--- main.c
+++ main.c
@@ -12,9 +12,9 @@
       return strncpy(buf, s + pos, count);
 } */
 
-char output[256];
 
 int main() {
+	char output[256]= {};
 	int fd[2];
 	char *listdoc[] = { "sh", "-c", "ls | grep .md", NULL};
 	char *sum[] = { "touch", "summary.html", NULL };
@@ -47,27 +47,30 @@ int main() {
 	}
 	char doc[mdn][256];
 	char title[mdn][256];
+	for(int i = 0; i < mdn; i++) {
+		*title[i] = '\0';
+	}
 	int k = 0;
 	int sb = 0, se=0;
 	for(int i = 0; i < strlen(output); i++) {
-		if(output[i] == '\n') {
+		if(output[i] == '\n' || output[i] == '\0') {
 			se = i;
-			if(k != 3) {
-				strncpy(doc[k], output+sb, se-sb);
+			strncpy(doc[k], output+sb, se-sb);
 			doc[k][se-sb] = '\0';
 			//printf("%s\n", doc[k]);
 			sb = i+1;
 			k++;
-			}
 		}
 	}
 	//printf("%d\n", mdn);
-	int fd2[2];
-	for(int i = 0; i < 255; i++) {
-		output[i] = 0;
-	}
-	output[255] = '\0';
+	//memset (output, 256, '\0' );
+	//*output = '\0';
+	/*for(int i = 0; i < mdn; i++) {
+		printf("%s\n", doc[i]);
+	}*/
 	for(int i = 0; i < mdn; i++) {
+		char output2[256] = {};
+		int fd2[2];
 		if(pipe(fd2) == -1) {
 			fprintf(stderr, "An error occurued creating pipe\n");
 			return 0;
@@ -81,7 +84,7 @@ int main() {
 			dup2(fd2[1], STDOUT_FILENO);
 			close(fd2[0]);
 			close(fd2[1]);
-			char arg[256];
+			char arg[256] = {};
 			strcpy(arg, "cat ");
 			strncat(arg, doc[i], strlen(doc[i]));
 			strcat(arg, " | grep title | cut -c 8-");
@@ -91,11 +94,14 @@ int main() {
 		}
 		else {
 			close(fd2[1]);
-			read(fd2[0], title[i], sizeof(title[i]));
-			//printf("%d\n", strlen(title[i]));
-			printf("%s\n", title[i]);
+			read(fd2[0], output2, sizeof(output2));
+			strncpy(title[i], output2, strlen(output2));
+			title[i][strlen(output2)-1] = '\0';
+			printf("%d\n", strlen(title[i]));
+			//printf("%s\n", title[i]);
+			//printf("Oops\n");
 		}
-		title[i][strlen(title[i])-2] = '\0';
+		title[i][strlen(title[i])] = '\0';
 	}
 	pid_t pid2 = fork();
 	if(pid2 == -1) {
