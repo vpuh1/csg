73adc19b8e947bbf99d75453b56be37ee6b972e8
Artem Baxtiarov <baxtiarovartem@gmail.com>
2021-07-28 00:39:22 +0300

    added license in convert.c convert.h and limits.h

--- src/limits.h
+++ src/limits.h
@@ -1,3 +1,21 @@
+/*
+ * csg -- simple static sites generator 
+ * Copyright (C) 2021 Artem Baxtiarov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef LIMITS_H
 #define LIMITS_H
 
